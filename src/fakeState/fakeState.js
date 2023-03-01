9cf4c940473d998d5150ffb56eab8a5439643d08
KindManl <aleksdorn69@gmail.com>
2023-03-01 11:14:08 +0700

    start redux + ui

---
+++ src/fakeState/fakeState.js
@@ -0,0 +1,14 @@
+
+let initialState = {
+    dialogs: [
+        {name: "alis", id: 0},
+        {name: "sasha", id: 1},
+        {name: "pasha", id: 2},
+        {name: "oleg", id: 3},
+        {name: "gavs", id: 4},
+    ]
+
+
+}
+
+export default initialState;
